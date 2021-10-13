3602553c72a8c69077bdee29ae4f682ab9e1cef9
JoseRinaldofilho <jose.vasconcelos@basis.com.br>
2021-10-13 13:34:10 -0300

    conclusao do modulo formulario

--- curso-php/formulario/formulario.php
+++ curso-php/formulario/formulario.php
@@ -2,42 +2,53 @@
 <div class="titulo">Formulario php</div>
 <h2>Cadastro</h2>
 <?php
+
+$erros = [];
+
 /**
  * validacao no nome string */
 if (!filter_var($_POST['nome'],FILTER_SANITIZE_FULL_SPECIAL_CHARS)) {
-    echo 'Nome InValido';
+    $erros['nome'] =  'Nome InValido';
 }
 // valida data filter IMPUT
 if (filter_input(INPUT_POST, "nascimento")) {
     $datas = DateTime::createFromFormat('d/m/y',$_POST['nascimento']);
     if ($datas) {
-        echo "Data deve esta no padrao dd/mm/aaaa <br>";
+        $erros['nascimento'] = "Data deve esta no padrao dd/mm/aaaa <br>";
     }
 }
     // se o email tiver vazio ou entrou formato mostra menssagem de error
     if (!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-        echo "Email Invalido <br>";
+        $erros['email'] ="Email Invalido <br>";
     }
     // valida o site
     if (!filter_var($_POST['site'],FILTER_VALIDATE_URL)) {
-        echo "Site Invalido <br>";
+        $erros['site'] ="Site Invalido <br>";
     }
      //valida filhos number
     $filhosConfig = ["options"=>["min_range"=>0,"max_range"=>20]];
     if (!filter_var($_POST['filhos'],FILTER_VALIDATE_INT, $filhosConfig) and $_POST['filhos'] !=0) {
-        echo "Quantidade de filhos invalida";
+        $erros['filhos'] ="Quantidade de filhos invalida";
     }
-    $salarioConfig =["options"=>['decimal'=>',']];
-    if (!filter_var($_POST['salario'],FILTER_VALIDATE_FLOAT,$salarioConfig)) {
-        echo "Salario Invalido";
+    $salarioConfig = ["options"=>['decimal'=>',']];
+    if (!filter_var($_POST['salario'],FILTER_VALIDATE_INT,$salarioConfig)) {
+        $erros['salario'] = "Salario Invalido";
     }
-
+        
 ?>
+<?php foreach ($erros as $errao):  ?>
+<!--    <div class="alert alert-danger" role="alert">-->
+<!--       --><?//= $errao ?>
+<!--    </div>-->
+<?php endforeach; ?>
 <form action="#" method="post">
     <div class="form-group">
         <label for="exampleInputEmail1">Nome</label>
-        <input type="text" name="nome" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nome"
+        <input type="text" name="nome" <?= $erros['nome']? 'is-invalid':''; ?>class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nome"
         value="<?= $_POST['nome'] ?>">
+        <div class="invalid-feedback">
+            <?php $erros['nome'] ?>
+        </div>
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1">Nascimento</label>
